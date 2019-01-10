83a53fe8386f01b346ff1f7196be4d8a1b01ab1b
dxf <dxf@itcast.cn>
2019-01-10 09:32:49 +0800

    品优购8.0

---
+++ pinyougou_parent/pinyougou_shop_web/src/main/webapp/js/controller/LoginController.js
@@ -0,0 +1,14 @@
+app.controller('loginController',function ($scope,loginService) {
+
+    $scope.loginName=function () {
+        loginService.loginName().success(
+            function (response) {
+                $scope.username=response;
+            }
+
+        )
+    }
+
+
+
+});
