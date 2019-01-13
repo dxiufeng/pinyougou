05566ca8a422d0030463bb124892dbad2cf92c33
dxf <dxf@itcast.cn>
2019-01-13 20:18:26 +0800

    品优购10 高亮显示

--- pinyougou_parent/pinyougou_search_service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou_parent/pinyougou_search_service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -5,15 +5,16 @@ import com.pinyougou.pojo.TbItem;
 import com.pinyougou.search.service.ItemSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.Query;
-import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.*;
+import org.springframework.data.solr.core.query.result.HighlightEntry;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.core.query.result.ScoredPage;
 
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 @Service(timeout = 5000)
 public class ItemSearchServiceImpl implements ItemSearchService {
 
@@ -24,7 +25,7 @@ public class ItemSearchServiceImpl implements ItemSearchService {
     //已经把数据库中的数据都添加到索引库中,并查询索引库
     @Override
     public Map<String, Object> search(Map<String, Object> searchMap) {
-        HashMap<String, Object> map = new HashMap<>();
+       /* HashMap<String, Object> map = new HashMap<>();
 
         //获取query查询对象
         Query query=new SimpleQuery("*:*");
@@ -39,9 +40,55 @@ public class ItemSearchServiceImpl implements ItemSearchService {
 
         List<TbItem> items = page.getContent();//获取数据的封装集合
 
-        map.put("rows", items);//封装到map集合中,rows表示每一行的数据,有多行
+        map.put("rows", items);//封装到map集合中,rows表示每一行的数据,有多行*/
 
 
+        //高亮结果显示
+        HashMap<String, Object> map = new HashMap<>();
+
+        //获取获取query查询对象
+        HighlightQuery query = new SimpleHighlightQuery();
+
+        //构建高亮对象
+        HighlightOptions highlightOptions = new HighlightOptions();
+        highlightOptions.addField("item_title");//添加高亮显示域
+        // highlightOptions.addField("item_price");//添加高亮显示域
+
+        //添加高亮效果
+        highlightOptions.setSimplePrefix("<en style='color:red'>");//前缀
+        highlightOptions.setSimplePostfix("</en>");//后缀
+
+        //query中设置高亮对象
+        query.setHighlightOptions(highlightOptions);
+
+        //添加查询条件
+        Criteria criteria = new Criteria("item_keywords");
+        criteria.is(searchMap.get("keywords"));
+        query.addCriteria(criteria);
+
+        //通过solrTemplate进行查询
+        HighlightPage<TbItem> page = solrTemplate.queryForHighlightPage(query, TbItem.class);
+
+        //高亮入口集合就是一些符合规则的高亮数据的集合[{id:1,item_title:"三星",},{},{}]
+        List<HighlightEntry<TbItem>> list = page.getHighlighted();
+
+        //循环遍历集合,设置title为高亮显示
+        //entry里面包含了highlightList和item
+        for (HighlightEntry<TbItem> entry : list) {
+
+            List<HighlightEntry.Highlight> highlightList = entry.getHighlights();//高亮显示结合
+
+            //判断是否存在高亮页
+            if (highlightList.size() > 0 && highlightList.get(0).getSnipplets().size() > 0) {
+
+                TbItem item = entry.getEntity();//获取高亮后的选项,一个entry就对应数据库里面一条数据
+                item.setTitle(highlightList.get(0).getSnipplets().get(0));
+
+            }
+        }
+
+        //把跳转好的数据放入map集合中返回
+        map.put("rows", page.getContent());
         return map;
     }
 }
--- pinyougou_parent/pinyougou_search_web/src/main/webapp/js/base.js
+++ pinyougou_parent/pinyougou_search_web/src/main/webapp/js/base.js
@@ -1,1 +1,11 @@
 var app = angular.module("pinyougou", []);//定义模块
+
+
+//定义过滤器
+app.filter('trustHtml',['$sce',function ($sce) {
+    return function (data) {//传入参数时被过滤的内容
+
+        return $sce.trustAsHtml(data)// 信任过滤后的内容
+
+    }
+}])
