6d5c309dfda9219a3ea6aad0945089935379eb6f
dxf <dxf@itcast.cn>
2019-01-14 20:51:14 +0800

    品优购10.3 构建查询条件

--- pinyougou_parent/pinyougou_search_web/src/main/webapp/js/controller/searchController.js
+++ pinyougou_parent/pinyougou_search_web/src/main/webapp/js/controller/searchController.js
@@ -1,5 +1,6 @@
 app.controller('searchController',function ($scope,searchService) {
 
+    //查询方法,通过solr进行查询,其中品牌和分类分别在redis中储存
     $scope.search=function () {
 
         searchService.search($scope.searchMap).success(
@@ -7,6 +8,34 @@ app.controller('searchController',function ($scope,searchService) {
                 $scope.resultMap=response;
             }
         )
+    }
+
+
+    //创建一个方法,用于操作面包屑导航栏
+    $scope.searchMap={'keywords':'','category':'','brand':'','spec':{}}; //初始化数据格式
+
+    //添加搜索条件
+    $scope.addSearchItem=function (key,value) {
+        if('category'==key || 'brand' == key){
+            //说明传入的数据是商品分类或者品牌分类
+            $scope.searchMap[key]=value;
+        }else {
+            //说明传入的数据是别的,即规格分类
+            $scope.searchMap.spec[key]=value;
+        }
+        //当进行修改后,调用search方法对后端进行访问
+        $scope.search()
+    }
+
+    //删除搜索条件
+    $scope.deleSearchItem=function (key) {
+        if('category'==key || 'brand'==key){
+            $scope.searchMap[key]='';
+        }else {
+            delete $scope.searchMap.spec[key];//移除此属性
+        }
+        //当进行修改后,调用search方法对后端进行访问
+        $scope.search()
     }
 
 });
